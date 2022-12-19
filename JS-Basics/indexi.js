bc593e930502e80658eb61f5d8e509c36cfd08ae
Atul Sathe <atulpsathes@outlook.com>
2022-12-19 21:40:37 +0530

    array for loop

--- JS-Basics/indexi.js
+++ JS-Basics/indexi.js
@@ -1,5 +1,12 @@
 //Array
-let selectedColours = ['red','blue'];
-selectedColours[2] = 2;
-console.log(selectedColours.length);
-
+//let selectedColours = ['red','blue'];
+//selectedColours[2] = 2;
+//console.log(selectedColours.length);
+const fruits =['apple', 'pear', 'mango'];
+let fruitlength = fruits.length;
+let text = "<ul>";
+for (let i=0; i<fruitlength; i++)
+{text += "<li>"+ fruits[i] + "</li>";
+}
+text += "</ul>"
+document.getElementById("demo").innerHTML = text;
